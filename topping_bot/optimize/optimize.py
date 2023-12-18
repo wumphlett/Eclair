84b93dcb4b990569b0f8cad95e53ea8956468f62
Will Humphlett <will@humphlett.net>
2023-12-18 06:31:56 -0800

    Uncomment testing change

--- topping_bot/optimize/optimize.py
+++ topping_bot/optimize/optimize.py
@@ -69,11 +69,11 @@ class Optimizer:
             )
 
     def best_objective(self, candidate: ToppingSet):
-        if self.solution is None or self.reqs.objective.value(candidate) > self.reqs.objective.value(self.solution):
-            tqdm.write(f":SOLUTION: {' | '.join([str(topping) for topping in candidate.toppings])}")
-            tqdm.write(str(candidate))
-            tqdm.write(str(self.reqs.objective.value(candidate)))
-            tqdm.write(str(self.reqs.objective.floor(candidate)))
+        # if self.solution is None or self.reqs.objective.value(candidate) > self.reqs.objective.value(self.solution):
+        #     tqdm.write(f":SOLUTION: {' | '.join([str(topping) for topping in candidate.toppings])}")
+        #     tqdm.write(str(candidate))
+        #     tqdm.write(str(self.reqs.objective.value(candidate)))
+        #     tqdm.write(str(self.reqs.objective.floor(candidate)))
         if self.solution is None:
             return candidate
         return max(self.solution, candidate, key=lambda x: self.reqs.objective.value(x))
